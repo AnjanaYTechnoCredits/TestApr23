22e9012b609dfa9cd2f835f67ca00f52a9dc0723
Anjana Yadav <anjana.yadav19@gmail.com>
2023-05-23 08:51:50 +0530

    Changes in A2

--- src/basicconcept/Example1.java
+++ src/basicconcept/Example1.java
@@ -3,6 +3,11 @@ package basicconcept;
 public class Example1 {
 	public static void main(String[] args) {
 		 System.out.println("welcome");
+		 
+		 
+		 int a=10;
+		 int b =20;
+		 System.out.print(a+b);
 		 System.out.println("inbranch B");
 		 System.out.println("Changes done");
 		 
