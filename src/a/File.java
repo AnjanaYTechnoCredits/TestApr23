056083b73626ed8c4e3a0f9b170b462f5196370a
Anjana Yadav <anjana.yadav19@gmail.com>
2023-05-23 09:38:41 +0530

    conflict created B3

--- src/a/File.java
+++ src/a/File.java
@@ -3,6 +3,7 @@ package a;
 public class File {
 	public static void main(String[] args) {
 		 System.out.println("welcome");
-		 System.out.println("in Branch B");
+		 System.out.println("Creating conflict in Branch B");
+		 System.out.println("added line");
 	 }
 }
